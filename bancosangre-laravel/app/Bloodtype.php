5c63d07ec44ec82052cced485fa316f68452efb4
fitty0930 <martinaguirre08@gmail.com>
2022-01-10 14:58:10 -0300

    somethign

--- bancosangre-laravel/app/Bloodtype.php
+++ bancosangre-laravel/app/Bloodtype.php
@@ -9,5 +9,8 @@ class Bloodtype extends Model
     /** TENGO QUE AVISARLE A LARAVEL QUE MI PRIMARY KEY SE LLAMA blood_id  */
     protected $primaryKey = 'blood_id'; 
     
-    // something
+    // something tstdfadshfia
+    /**
+     * fdasjhfodjasofa
+     */
 }
