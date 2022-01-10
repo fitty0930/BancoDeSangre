6624b191e34cdcf1bc00ec295be72d623b73b614
fitty0930 <martinaguirre08@gmail.com>
2022-01-09 22:23:53 -0300

    minor change

--- bancosangre-laravel/app/Bloodtype.php
+++ bancosangre-laravel/app/Bloodtype.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Bloodtype extends Model
 {
-    // TENGO QUE AVISARLE A LARAVEL QUE MI PRIMARY KEY SE LLAMA blood_id
+    /** TENGO QUE AVISARLE A LARAVEL QUE MI PRIMARY KEY SE LLAMA blood_id  */
     protected $primaryKey = 'blood_id'; 
     
     //
